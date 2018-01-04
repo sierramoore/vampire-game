2b7ba99abf68aa7bcd9c81552b68558774f58791
Sierra Moore <sierra.v.moore@gmail.com>
2018-01-04 16:47:11 -0600

    replaced dot with vampire image

--- app.js
+++ app.js
@@ -11,42 +11,53 @@ let isOverlapped = function(x1,y1,w1,h1,x2,y2,w2,h2){
 };
 
 const vamp = {
+    x: 40,
+    y:400,
+    w: 40,
+    h: 40,
     body: {},
     direction: "",
     initVamp: function (){
         this.body = {x: 40, y: 400, r: 12.5, e: 0}
     },
-    drawBody: function(){
-    	ctx.beginPath();
-        ctx.fillStyle = "white";
-    	ctx.arc(vamp.body.x, vamp.body.y, vamp.body.r, vamp.body.e, Math.PI*2);
-    	ctx.fill();
-    	ctx.closePath();
+    vampImage: function (){
+        let img = new Image();
+        img.src = "images/vampy.png";
+        // img.src = "https://st.depositphotos.com/3248189/4472/v/950/depositphotos_44727327-stock-illustration-cartoon-vampire-bat.jpg";
+        ctx.drawImage(img, this.x, this.y, this.w, this.h)
     },
+    // drawBody: function(){
+    // 	ctx.beginPath();
+    //     ctx.fillStyle = "white";
+    // 	ctx.arc(vamp.body.x, vamp.body.y, vamp.body.r, vamp.body.e, Math.PI*2);
+    // 	ctx.fill();
+    // 	ctx.closePath();
+    // },
     //set space between windows
     //set range of windows to be generated with boundries
     //if within canvas boundry -> if statements
      move: function(){
-    	if(vamp.direction === 'right'){
-    		if(vamp.body.x + 20 < 800){
-    		    // move hero right along x axis
-    		vamp.body = {x: vamp.body.x + 10, y: vamp.body.y, r: 12.5, e:0}
+    	if(this.direction === 'right'){
+    		if(this.x + 20 < 800){
+    		    // move right along x axis
+                this.x += 10;
     		}
-    		
-    	}else if(vamp.direction === "left"){
-            if(vamp.body.x >= 20)
+    	}
+    	else if(this.direction === "left"){
+            if(this.x >= 20)
             // move left from x axis
-    		vamp.body = {x: vamp.body.x - 10, y: vamp.body.y, r: 12.5, e:0}
+    		this.x -= 10;
 
-    	}else if(vamp.direction === "up"){
+    	}else if(this.direction === "up"){
     	    //set to 200px so cant move above dark area
-    	    if(vamp.body.y >=200)
-    		vamp.body = {x: vamp.body.x, y: vamp.body.y - 10, r: 12.5, e:0}
+    	    if(this.y >=200)
+    	        this.y -= 10;
 
-    	}else if(vamp.direction === "down"){
+    	}else if(this.direction === "down"){
     	    //set to only move within dark grey area
-    	    if(vamp.body.y <= 600)
-    		vamp.body = {x: vamp.body.x, y: vamp.body.y + 10, r: 12.5, e:0}
+    	    if(this.y <= 600) {
+                this.y += 10;
+            }
     	}
     }
 };
@@ -91,12 +102,13 @@ const game = {
     windows: [],
     // function to set random values and output/return/store window object in array of windows
     generateWindow: function () {
+        ctx.globalCompositeOperation='destination-over';
         //set properties
         let x = 700;
         //if y between red borders
         let y = Math.floor(Math.random() * 400) + 80;
         let width = Math.floor(Math.random() * 100) + 70;
-        let height = Math.floor(Math.random() * 150) + 120;
+        let height = Math.floor(Math.random() * 250) + 120;
 
         let window = {
             x: x,
@@ -127,7 +139,7 @@ const game = {
         for(let i=0; i < this.windows.length; i++){
             let xValue = this.windows[i].x;
             //update value of x in window arr
-            this.windows[i].x = xValue -3;
+            this.windows[i].x = xValue -4;
         }
     },
     addWindows: function() {
@@ -208,14 +220,14 @@ const animateCanvas = function(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     // this causes the vamp to show on the screen
-    vamp.drawBody();
+    // vamp.drawBody();
+    vamp.vampImage();
 
     game.drawScore();
 
     game.drawHealthBar();
 
     if(game.health === 0){
-
         ctx.font = "80px 'Berkshire Swash'";
         ctx.fillStyle = "red";
         ctx.fillText("Game Over",200,400);
@@ -235,7 +247,6 @@ vamp.initVamp();
 
 animateCanvas();
 
-//set event listeners to catch user interaction and set animation function
 
 
 
